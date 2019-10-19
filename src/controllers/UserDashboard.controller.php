c91c83b6f88c2c63270df45f266abb341454293b
masood <m.amiri.t@gmail.com>
2019-10-19 10:22:15 +0330

    delete user dashboard

--- src/controllers/UserDashboard.controller.php
+++
@@ -1,6 +0,0 @@
-<?php
-//todo: main page controller, handles dashboard contents with functions from resume models
-//todo: show request sent items
-//todo: show related companies
-//todo: send resume for selected company and show message sent or failed
-
